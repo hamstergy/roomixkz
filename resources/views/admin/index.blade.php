a76d602b759134e0d75d68749270dfa59df9ec2b
Yuriy Goryachev <Yuriy Goryachev>
2017-06-07 23:47:05 +0600

    admin panel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Юрий
- * Date: 04.06.2017
- * Time: 22:27
- */
+@extends('/layouts/admin')
+
+@section('content')
+    <h1>Navbar example</h1>
+    <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+    <p>
+        <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a>
+    </p>
+@stop
